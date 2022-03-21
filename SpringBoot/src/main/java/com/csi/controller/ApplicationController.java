5f6cbf985c8fce4eb75c2a4f8543054785576f6b
poonamdodake <poonamdodake@gmail.com>
2022-03-21 14:16:38 +0530

    initial commit

---
+++ SpringBoot/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,22 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class ApplicationController {
+    @GetMapping
+
+    public String sayWelcome()
+    {
+        return "welcome to finetch csi";
+    }
+
+    @GetMapping("/Services")
+    public String  sayServices()
+    {
+        return  "Software Development";
+    }
+}
